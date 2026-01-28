e6fbe5dcb6980e48974e938bb1d9b187eb73bad5
Joanna Raveloson <joanna.raveloson.etu@unistra.fr>
2026-01-28 22:21:32 +0100

    Ajout API

---
+++ api/config.js
@@ -0,0 +1,10 @@
+module.exports = {
+    ACCESS_TOKEN_SECRET: "EMMA123",
+    BDD: {
+        "host": "dpg-d5mfidkoud1c739bkavg-a.oregon-postgres.render.com",
+        "port": "5432",
+        "user": "projetdb_vnxu_user",
+        "password": "9132ANc2puv9xnNeIEwTnrbI1jzcAWFi",
+        "bdname": "projetdb_vnxu"
+    }
+}
